8bc2f2f2dc7f5fb0175d23c9d93739c07919c334
Francisco J. Morales <fjmoralespiqueras@gmail.com>
2019-06-07 20:04:56 +0200

    Corrige cosas

--- main.py
+++ main.py
@@ -38,6 +38,20 @@ def score_f(y_true, y_pred):
 scorer = make_scorer(score_f)
 
 
+def resample(X, y):
+    X_res, y_res = list(X), list(y)
+    for x, c in zip(X, y):
+        if c == 1:
+            for i in range(49):
+                X_res.append(x)
+                y_res.append(-1)
+    X_res, y_res = np.array(X_res), np.array(y_res)
+    np.random.shuffle(X_res)
+    np.random.shuffle(y_res)
+
+    return X_res, y_res
+
+
 def compare_models(dataset, models):
     scores = {}
     max_score = 0
@@ -69,12 +83,10 @@ def tune_parameters(classifier, parameters, dataset, scorer, n_iter=10,
 
     classifier.fit(dataset.train_var, dataset.train_output)
 
-    y_true, y_pred = dataset.test_output, classifier.predict(dataset.test_var)
-    score = score_f(y_true, y_pred)
+    print(f"Mejores parametros ({classifier.best_score_}): "
+          f"{classifier.best_params_}")
 
-    print(f"Mejores parametros ({score}): {classifier.best_params_}")
-
-    return classifier, score
+    return classifier
 
 
 def model_file(name):
@@ -90,9 +102,9 @@ def load_model(name):
 
 
 def load_all_models(model_names):
-    models = []
+    models = {}
     for name in model_names:
-        models.append(load_model(name))
+        models[name] = load_model(name)
 
     return models
 
@@ -125,12 +137,12 @@ def tuning():
         'pct__alpha': np.logspace(-5, -1, num=5, base=10),
     }
 
-    # 3x20 = 398s
-    # start_time = time.time()
-    # pct_clf, score = tune_parameters(pct_clf, pct_parameters, ds, scorer,
-    #                                  verbose=True, n_iter=50)
-    # print("--- %s seconds ---" % (time.time() - start_time))
-    # save_model(pct_clf, 'Perceptron')
+    # {'pct__penalty': 'elasticnet', 'pct__alpha': 0.0001, 'pca__n_components': 0.95}
+    start_time = time.time()
+    pct_clf = tune_parameters(pct_clf, pct_parameters, ds, scorer,
+                              verbose=True, n_iter=30)
+    print("--- %s seconds ---" % (time.time() - start_time))
+    save_model(pct_clf, 'Perceptron CV')
 
     # Neural network
     nn_clf = Pipeline(steps=[
@@ -147,13 +159,12 @@ def tuning():
         'mlp__learning_rate': ['constant', 'adaptive']
     }
 
-    # 3x10 = 1469
     start_time = time.time()
-    # # {'pca__n_components': 0.95, 'mlp__learning_rate': 'constant',
-    # # 'mlp__hidden_layer_sizes': (100,), 'mlp__alpha': 0.1, 'mlp__
-    # # activation': 'relu'}
-    # nn_clf, _ = tune_parameters(nn_clf, nn_parameters, ds, scorer, verbose=True)
-    # save_model(nn_clf, 'NeuralNetwork')
+    # {'pca__n_components': 0.95, 'mlp__learning_rate': 'constant',
+    # 'mlp__hidden_layer_sizes': (100,), 'mlp__alpha': 0.1, 'mlp__
+    # activation': 'relu'}
+    nn_clf = tune_parameters(nn_clf, nn_parameters, ds, scorer, verbose=True)
+    save_model(nn_clf, 'Neural Network CV')
     # print("--- %s seconds ---" % (time.time() - start_time))
 
     # AdaBoost
@@ -169,32 +180,30 @@ def tuning():
     }
 
     # # 3x5 = 3566s
-    # start_time = time.time()
-    # # {'pca__n_components': 0.8, 'ab__learning_rate': 0.1}
-    # ab_clf, score = tune_parameters(ab_clf, ab_parameters, ds, scorer, verbose=True)
-    # save_model(ab_clf, 'AdaBoost')
-    # print("--- %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
+    # {'pca__n_components': 0.8, 'ab__learning_rate': 0.1}
+    ab_clf = tune_parameters(ab_clf, ab_parameters, ds, scorer, verbose=True)
+    save_model(ab_clf, 'AdaBoost CV')
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     # Random Forest
 
     rf_clf = Pipeline(steps=[
         ('pca', PCA(svd_solver='full')),
         ('poly', PolynomialFeatures(2)),
-        ('rf', RandomForestClassifier(max_features='sqrt'))
+        ('rf', RandomForestClassifier(max_features='sqrt', criterion='gini'))
     ])
 
     rf_parameters = {
         'pca__n_components': [.80, .90, .95, 1],
         'rf__n_estimators': [10, 40, 160],
-        'rf__criterion': ['gini', 'entropy']
     }
 
-    # # 3 x 24
-    # start_time = time.time()
-    # # {'rf__n_estimators': 160, 'rf__criterion': 'gini', 'pca__n_components': 0.8}
-    # rf_clf, _ = tune_parameters(rf_clf, rf_parameters, ds, scorer, verbose=True, n_iter=50)
-    # save_model(rf_clf, 'RandomForest')
-    # print("--- %s seconds ---" % (time.time() - start_time))
+    start_time = time.time()
+    # {'rf__n_estimators': 160, 'rf__criterion': 'gini', 'pca__n_components': 0.8}
+    rf_clf = tune_parameters(rf_clf, rf_parameters, ds, scorer, verbose=True, n_iter=12)
+    save_model(rf_clf, 'RandomForest CV')
+    print("--- %s seconds ---" % (time.time() - start_time))
 
     models = {
         "Perceptron": pct_clf,
@@ -202,6 +211,68 @@ def tuning():
         "AdaBoost": ab_clf,
         "Random Forest": rf_clf
     }
+
+    return models
+
+
+def train():
+    pct_clf = Pipeline(steps=[
+        ('pca', PCA(svd_solver='full', n_components=0.95)),
+        ('poly', PolynomialFeatures(2)),
+        ('pct', Perceptron(penalty='elasticnet', alpha=.0001, max_iter=500,
+                           tol=.001, class_weight=ds.WEIGHTS_DIC))
+    ])
+
+    start_time = time.time()
+    pct_clf.fit(ds.train_var, ds.train_output)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    train_var_res, train_output_res = resample(ds.train_var, ds.train_output)
+
+    nn_clf = Pipeline(steps=[
+        ('pca', PCA(svd_solver='full', n_components=0.95)),
+        ('poly', PolynomialFeatures(2)),
+        ('mlp', MLPClassifier(solver='adam', max_iter=100, tol=0.001,
+                              learning_rate='constant',
+                              hidden_layer_sizes=(100,), alpha=0.1,
+                              activation='relu'))
+    ])
+
+    start_time = time.time()
+    nn_clf.fit(train_var_res, train_output_res)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    ab_clf = Pipeline(steps=[
+        ('pca', PCA(svd_solver='full', n_components=0.8)),
+        ('poly', PolynomialFeatures(2)),
+        ('ab', AdaBoostClassifier(learning_rate=0.1))
+    ])
+
+    start_time = time.time()
+    ab_clf.fit(train_var_res, train_output_res)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    rf_clf = Pipeline(steps=[
+        ('pca', PCA(svd_solver='full', n_components=0.8)),
+        ('poly', PolynomialFeatures(2)),
+        ('rf', RandomForestClassifier(max_features='sqrt', n_estimators=160,
+                                      criterion='gini',
+                                      class_weight=ds.WEIGHTS_DIC))
+    ])
+
+    start_time = time.time()
+    rf_clf.fit(ds.train_var, ds.train_output)
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    models = {
+        "Perceptron": pct_clf,
+        "Neural network": nn_clf,
+        "AdaBoost": ab_clf,
+        "Random Forest": rf_clf
+    }
+
+    for model_name in models:
+        save_model(models[model_name], model_name)
 
     return models
 
@@ -219,17 +290,36 @@ def yes_or_no(question):
     return ans
 
 
-do_tuning = yes_or_no("¿Desea hacer la estimación de los hiperparámetros para "
-                      "cada modelo? (Puede tardar algunas horas) [Sí/No] ")
+def main():
+    do_tuning = yes_or_no("¿Desea hacer la estimación de los hiperparámetros "
+                          "para cada modelo? (Puede tardar algunas horas) "
+                          "[Sí/No] ")
 
-if do_tuning:
-    tuning()
-else:
-    do_training = yes_or_no("¿Desea hacer el entrenamiento con los parámetros "
-                            "que figuran en la memoria de la práctica? (Puede "
-                            "tardar unos minutos; alternativamente puede leer "
-                            "los modelos ya entrenados) [Sí/No] ")
-    if do_training:
-        pass
+    if do_tuning:
+        models = tuning()
     else:
-        print("Procederemos a leer los modelos ya entrenados.")
+        do_training = yes_or_no("¿Desea hacer el entrenamiento con los "
+                                "parámetros que figuran en la memoria de la "
+                                "práctica? (Puede tardar unos minutos; "
+                                "alternativamente puede leer los modelos ya "
+                                "entrenados) [Sí/No] ")
+        if do_training:
+            models = train()
+        else:
+            print("Procederemos a leer los modelos ya entrenados.")
+            models = load_all_models(("Perceptron", "Neural network",
+                                      "AdaBoost", "Random Forest"))
+
+    for model_name in models:
+        model = models[model_name]
+        score = model.score(ds.test_var, ds.test_output,
+                            sample_weight=ds.get_sample_weight_test())
+        score_train = model.score(ds.train_var, ds.train_output,
+                                  sample_weight=ds.get_sample_weight_train())
+        preds = model.predict(ds.test_var)
+        print(score_f(ds.test_output, preds))
+        print(f"Precisión ponderada del modelo {model_name}: "
+              f"{score, score_train}", )
+
+
+main()
